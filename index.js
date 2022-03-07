27fa6429e23b6217f7c8853796af10e654be8978
Jefriwahyu <memberjefry1@gmail.com>
2022-03-07 19:44:50 +0700

    Tugas 8 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,26 @@
+function barang(){
+  var jerseyBola = {
+    klub: 'Manchester United',
+    jersey: 'Home',
+    brand: 'Adidas',
+    warna: 'Merah',
+    buatan: 'Indonesia',
+    tahun: 2021,
+    harga: 'Rp. 799.000,00'
+  }
+  var jerseyBola1 = {
+    klub: 'Timnas Indonesia',
+    jersey: 'Home',
+    brand: 'Mils',
+    warna: 'Merah',
+    buatan: 'Indonesia',
+    tahun: 2021,
+    harga: 'Rp. 999.000,00'
+  }
+  jerseyBola.tahun = 2022;
+  jerseyBola1.harga = "Rp. 699.000,00"
+  console.log(jerseyBola)
+  console.log(jerseyBola1)
+}
+
+barang()
